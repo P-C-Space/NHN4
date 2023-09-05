4553143a4acdc79836e35d9da40d36b1c41fea1e
jeongwoo <sun62073812@gmail.com>
2023-09-05 17:32:57 +0900

    틱택토 수정

--- ViceDean/nhn20230904/TicTacToe.java
+++ ViceDean/nhn20230904/TicTacToe.java
@@ -76,13 +76,14 @@ public class TicTacToe {
         for (int i = 0; i < 3; i++) {
             if (((board[i][0] == board[i][1] && board[i][1] == board[i][2])
                     || (board[0][i] == board[1][i] && board[1][i] == board[2][i])) && board[i][i] != ' ') {
-                System.out.println("축하합니다 승리자 : " + board[i][i]);
+                System.out.println("축하합니다! 승리자 : " + board[i][i]);
                 return true;
             }
         }
 
         if (((board[0][0] == board[1][1] && board[1][1] == board[2][2])
                 || (board[0][2] == board[1][1] && board[1][1] == board[2][0])) && board[1][1] != ' ') {
+                    System.out.println("축하합니다! 승리자 : " + board[1][1]);
             return true;
         }
 
