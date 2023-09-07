f606e300bf76fb0455bd10758fc813cdf0ee3016
jeongwoo <sun62073812@gmail.com>
2023-09-07 10:26:34 +0900

    부학장님 오전 수업 1

---
+++ ViceDean/nhn20230907/POKER/src/com/nhnacademy/java/poker/Card.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.java.poker;
+
+public class Card{
+    
+}
---
+++ ViceDean/nhn20230907/POKER/src/com/nhnacademy/java/poker/Date.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.java.poker;
+
+public class Date {
+    private int year;
+    private int month;
+    private int day;
+    
+    public Date(int year,int month,int day){
+        
+    }
+}
